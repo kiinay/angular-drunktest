f7ad9996f7bb08f08940f9a7eba1558aa74f6066
Yannick PLACERDAT <yannick.placerdat@gmail.com>
2015-05-15 17:11:37 +0200

    Display questions and theirs propose but is not finished

--- app/scripts/controllers/test.js
+++ app/scripts/controllers/test.js
@@ -2,13 +2,25 @@
 
 /**
  * @ngdoc function
- * @name drunktestApp.controller:MainCtrl
+ * @name drunktestApp.controller:TestCtrl
  * @description
- * # MainCtrl
+ * # TestCtrl
  * Controller of the drunktestApp
  */
 
 angular.module('drunktestApp')
   .controller('TestCtrl', function () {
+    var questions = [
+      { question: 'Dans une course à pieds, je double le 2ème ! Je suis donc :', propose: ['Dernier', 'Troisième', 'Premier'], answer: 'Premier'},
+      { question: 'Dans une course à pieds, je double le 2ème ! Je suis donc :', propose: ['Dernier', 'Troisième', 'Premier'], answer: 'Premier'},
+      { question: 'Dans une course à pieds, je double le 2ème ! Je suis donc :', propose: ['Dernier', 'Troisième', 'Premier'], answer: 'Premier'}
+    ];
 
+    var go = {
+      value: false
+    };
+
+    this.listeQuestions = questions;
+
+    this.next = go;
   });
